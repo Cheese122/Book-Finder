ed4cfa718b6f5331651adc39f965af3c55535f1a
Cheese122 <48573895+Cheese122@users.noreply.github.com>
2020-10-07 15:16:04 +1100

    Replaced sleeps with Input

--- BookFinderMain.py
+++ BookFinderMain.py
@@ -7,14 +7,14 @@ import random
 def LookupISBN(ID):
         if isbnlib.is_isbn13(ID):
                 trimmedID = isbnlib.canonical(ID)
-                webbrowser.open("https://isbnsearch.org/isbn/{}".format(trimmedID)) #TODO: LOOKUP ID ON AMAZON OR SOMETHING
+                webbrowser.open("https://isbnsearch.org/isbn/{}".format(trimmedID)) 
         else:
                 if isbnlib.is_isbn10(ID):
                         trimmedIdISBN10 = isbnlib.canonical(ID)
                         webbrowser.open("https://isbnsearch.org/isbn/{}".format(trimmedIdISBN10))
                 else: 
                         print("Invalid input. Please check to see if it is in either ISBN13 or ISBN10.")
-                        time.sleep(3)
+                        input()
                         exit()
 
 
@@ -37,16 +37,16 @@ choice = input("Please make a selection\n")
 if choice == "1": 
         isbnId = input("Please input your ISBN:\n")
         LookupISBN(isbnId)
-        time.sleep(60)
+        input()
 
 
 elif choice == "2":
         RandomBookLookup()
-        time.sleep(60)
-        #TODO: RANDOM BOOK ID
+        input()
+        
 else:
         print("Invalid input. This terminal will close.")
-        time.sleep(3)
+        input()
         exit()
 
 
